0b9dac4526ae6ca1a04ba0f36f028c2cfb3a8864
meilleur04 <102686601+meilleur04@users.noreply.github.com>
2022-04-05 21:45:41 +0530

    commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1 className="container mx-auto text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
