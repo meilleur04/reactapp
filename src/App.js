2959e76b33b8876e261ccc14a9acc305a6516090
meilleur04 <102686601+meilleur04@users.noreply.github.com>
2022-04-06 06:46:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/HeadPage';
+import Products from './components/Products';
 
 function App() {
   return (
     <>
     <Navbar title="Jaggi Industries"/>
+    <Products/>
     </>
   );
 }
