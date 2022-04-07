f38b1d645bb3930c256112fcfd54da9ac2ebea02
meilleur04 <102686601+meilleur04@users.noreply.github.com>
2022-04-07 19:20:04 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,7 @@ import { MenuIcon, XIcon } from '@heroicons/react/outline'
 const navigation = [
   {name: 'Home', href: '#'},
   { name: 'Products', href: '#' },
-  { name: 'About', href: '#' },
-  { name: 'Contact', href: '#' },
-
+  { name: 'About us & Contact us', href: '#' },
 ]
 
 export default function Navbar() {
@@ -28,7 +26,7 @@ export default function Navbar() {
           </svg>
 
           <Popover>
-            <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+            <div className="relative pt-6 px-2 sm:px-2 lg:px-4">
               <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
                 <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                   <div className="flex items-center justify-between w-full md:w-auto">
@@ -47,7 +45,7 @@ export default function Navbar() {
                     </div>
                   </div>
                 </div>
-                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                <div className="hidden md:block md:ml-5 md:pr-5 md:space-x-8">
                   {navigation.map((item) => (
                     <a key={item.name} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
                       {item.name}
