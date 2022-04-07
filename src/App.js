fe1a9b4c7119ac4f8abcded05b19b44a62655588
meilleur04 <102686601+meilleur04@users.noreply.github.com>
2022-04-07 18:43:55 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Products from './components/Products';
 import HeadPage from './components/HeadPage';
 import Navbar from './components/Navbar';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <>
     <Navbar/>
-    <Products/>
     </>
   );
 }
